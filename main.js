41c70cdae7470a8d15e2d9fbf4d30c9251dece23
Leyber91 <leyber.91.2@gmail.com>
2021-02-07 19:22:26 +0100

    exercise 5 done

--- main.js
+++ main.js
@@ -50,6 +50,97 @@ displayer.appendChild(btnEx2)
 
 
 
+// EXERCISE 3 CODE
+let btnEx3 = document.createElement('button')
+let divEx3 = document.createElement('div')
+btnEx3.className = 'exercise3'
+btnEx3.textContent = 'Exercise 3'
+//DEFINE FUNCTION
+let exercise3 = function() {
+//INSERT CODE HERE TO EXECUTE
+let nom = 'Luis'
+let cognom = 'Blanco Rodriguez'
+console.log(`Mi nombre es : ${nom}`)
+console.log(`Mis apellidos son : ${cognom}`)
+
+}
+btnEx3.addEventListener('click', exercise3)
+//DEFINE EXERCISE 3 TITLE AND STATEMENT
+let Ex3Title =document.createElement('h2')
+Ex3Title.className = 'title'
+Ex3Title.textContent = 'Exercise 3'
+let Ex3Statement = document.createElement('p')
+Ex3Statement.className = 'statement'
+Ex3Statement.textContent = `Crea una variable que contingui el teu nom i un altre que contingui el teu cognom.`
+//APPEND EXERCISE 3 FUNCTION
+displayer.appendChild(Ex3Title)
+displayer.appendChild(Ex3Statement)
+displayer.appendChild(btnEx3)
 
 
+// EXERCISE 4 CODE
+let btnEx4 = document.createElement('button')
+let divEx4 = document.createElement('div')
+btnEx4.className = 'exercise4'
+btnEx4.textContent = 'Exercise 4'
+//DEFINE FUNCTION
+let exercise4 = function() {
+//INSERT CODE HERE TO EXECUTE
+let a = 42
+let b = 23
+console.log(`La suma de 42 y 23 es : ${a + b}`)
+}
+btnEx4.addEventListener('click', exercise4)
+//DEFINE EXERCISE 4 TITLE AND STATEMENT
+let Ex4Title =document.createElement('h2')
+Ex4Title.className = 'title'
+Ex4Title.textContent = 'Exercise 4'
+let Ex4Statement = document.createElement('p')
+Ex4Statement.className = 'statement'
+Ex4Statement.textContent = `Crea dos variables amb dos números i fes una suma entre ells.`
+//APPEND EXERCISE 4 FUNCTION
+displayer.appendChild(Ex4Title)
+displayer.appendChild(Ex4Statement)
+displayer.appendChild(btnEx4)
+
+
+// EXERCISE 5 CODE
+let btnEx5= document.createElement('button')
+let divEx5 = document.createElement('div')
+let inputEx5 = document.createElement('input')
+inputEx5.id = 'nota'
+btnEx5.className = 'exercise5'
+btnEx5.textContent = 'Submite tu nota'
+//DEFINE FUNCTION
+let exercise5 = () => {
+//INSERT CODE HERE TO EXECUTE
+
+    let x = document.getElementById('nota').value
+    if (x >= 5 && x <= 10){
+        return alert(`'Enhorabuena, has aprovado con un ${x}`)
+    } else if (x >= 5 ){
+        return alert(`'Va a ser que no has aprovado, obtuviste ${x}`)
+    } else {
+        return alert(`${x} no es una nota real`)
+    }
+}
+
+btnEx5.addEventListener('click', exercise5)
+
+//btnEx5__1.addEventListener('click', exercise5(btnEx5__1.textContent))
+//btnEx5__2.addEventListener('click', exercise5(btnEx5__1.textContent))
+//btnEx5__3.addEventListener('click', exercise5(12))
+//DEFINE EXERCISE 5 TITLE AND STATEMENT
+let Ex5Title =document.createElement('h2')
+Ex5Title.className = 'title'
+Ex5Title.textContent = 'Exercise 5'
+let Ex5Statement = document.createElement('p')
+Ex5Statement.className = 'statement'
+Ex5Statement.textContent = `Crea una variable nota_examen juntament amb un alert que ens digui si l’examen ha estat aprovat o no juntament amb la nota.`
+//APPEND EXERCISE 5 FUNCTION
+displayer.appendChild(Ex5Title)
+displayer.appendChild(Ex5Statement)
+divEx5.appendChild(inputEx5)
+divEx5.appendChild(btnEx5)
+displayer.appendChild(divEx5)
 
